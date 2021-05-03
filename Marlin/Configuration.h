9f9286fd9858efe5418e2253c9d4b563e829d627
uberjuice <jagesquirrel@gmail.com>
2021-05-03 21:51:47 +0200

    removed ENABLE_LEVEING_FADE_HEIGHT

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1226,7 +1226,7 @@
   // Gradually reduce leveling correction until a set height is reached,
   // at which point movement will be level to the machine's XY plane.
   // The height can be set with M420 Z<height>
-  #define ENABLE_LEVELING_FADE_HEIGHT
+  //#define ENABLE_LEVELING_FADE_HEIGHT
 
   // For Cartesian machines, instead of dividing moves on mesh boundaries,
   // split up moves into short segments like a Delta. This follows the
@@ -1334,7 +1334,7 @@
  * Commands to execute at the end of G29 probing.
  * Useful to retract or move the Z probe out of the way.
  */
-#define Z_PROBE_END_SCRIPT "G1 Z10 X15 Y15" // CHANGED FROM UNCOMENTED
+//#define Z_PROBE_END_SCRIPT "G1 Z10 X15 Y15" // CHANGED FROM UNCOMENTED
 
 
 // @section homing
