6d3041f92858bbd82dc98053b781a32dd1f949aa
uberjuice <jagesquirrel@gmail.com>
2021-05-04 18:30:38 +0200

    set different Z-offset (0.3) from 0.2 (eeprom)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -956,7 +956,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { 32.3, 11.7, -0.6 }
+#define NOZZLE_TO_PROBE_OFFSET { 32.3, 11.7, -0.3 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
